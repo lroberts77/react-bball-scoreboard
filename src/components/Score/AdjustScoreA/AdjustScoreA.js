5fc7a72d64f024ebb918ebfda55256773b97a1ec
Liam R <liamroberts77@googlemail.com>
2022-06-08 17:02:09 +0100

    added logic to adjust teamb score

--- src/components/Score/AdjustScoreA/AdjustScoreA.js
+++ src/components/Score/AdjustScoreA/AdjustScoreA.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ScoreContext } from '../../../Context/Context'
 import './AdjustScoreA.css'
 
 const AdjustScoreA = () => {
+
+  let { scoresTeamA, setScoresTeamA, scoresTeamB, setScoresTeamB } = useContext(ScoreContext)
+
   return (
     <div className='scoreA'>AdjustScoreA</div>
   )
--- src/components/Score/AdjustScoreB/AdjustScoreB.js
+++ src/components/Score/AdjustScoreB/AdjustScoreB.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ScoreContext } from '../../../Context/Context'
 import './AdjustScoreB.css'
 
 const AdjustScoreB = () => {
+
+  let { scoresTeamA, setScoresTeamA, scoresTeamB, setScoresTeamB } = useContext(ScoreContext)
+
   return (
-    <div className='scoreB'>AdjustScoreB</div>
+    <div className='scoreB'>
+          <button id="increment-btnb" onClick={() => setScoresTeamB( scoresTeamB => scoresTeamB + 1 )}>+1</button>
+          <button id="increment3-btnb" onClick={() => setScoresTeamB( scoresTeamB => scoresTeamB + 3 )}>+3</button>
+          <button id="decrement-btnb" onClick={() => scoresTeamB <= 0 ? scoresTeamB => scoresTeamB = 0 : setScoresTeamB( scoresTeamB => scoresTeamB - 1 )}>-1</button>
+          <button id="reset-btnb" onClick={() => setScoresTeamB( scoresTeamB => scoresTeamB = 0 )}>Reset</button>    
+    </div>
   )
 }
 
